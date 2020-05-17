411c6838b13da594ddf03a211b3c6f4a8445dca1
Nathan Nguyen <nathannguyen345@gmail.com>
2020-05-16 18:28:48 -0700

    Added workflow selector component

---
+++ client/src/components/form/WorkflowForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WorkflowForm() {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+}
+
+export default WorkflowForm;
--- client/src/components/selector/WorkflowSelector.js
+++ client/src/components/selector/WorkflowSelector.js
@@ -1,13 +1,60 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
+import WorkflowForm from "../form/WorkflowForm";
 
 function WorkflowSelector() {
+  const [workflows, setWorkflows] = useState([]);
+  const [id, setId] = useState("");
+  const selectRef = useRef(null);
+
+  // Call Express API to retreive workflows run on mount
+  useEffect(() => {
+    selectRef.current.focus();
+    axios
+      .get("/api/getWorkflows")
+      .then((response) => {
+        setWorkflows(response.data["userWorkflowList"]);
+        setId(response.data["userWorkflowList"][0]["workflowId"]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  // Map all workflows to options for selection
+  const mapWorkflows = workflows.map((workflow) => (
+    <option key={workflow.workflowId} value={workflow.workflowId}>
+      {workflow.displayName}
+    </option>
+  ));
+
+  // Handle onChange of selection
+  const handleSelectChange = (e) => {
+    setId(e.target.value);
+  };
+
+  // Handle the submit button
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setSubmit(true);
+  };
+
   return (
     <div className="jumbotron">
-      <div className="form-group">
-        <label>Work Flow Selector</label>
-        <select></select>
-      </div>
+      <form onSubmit={submitHandler}>
+        <div className="form-group">
+          <label>Work Flow Selector</label>
+          <select
+            className="form-control"
+            id="workflow-dropdown"
+            ref={selectRef}
+            onChange={handleSelectChange}
+          >
+            {mapWorkflows}
+          </select>
+        </div>
+        <button>Select</button>
+      </form>
     </div>
   );
 }
